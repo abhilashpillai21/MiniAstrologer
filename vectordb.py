83662615424066f727a577564342ad0e6c3a339e
abhilashpillai21 <abhilashpillai21@gmail.com>
2026-04-25 15:40:05 +0000

    Removed arguments to OpenAI()

--- vectordb.py
+++ vectordb.py
@@ -10,7 +10,7 @@ from openai import OpenAI
 import hashlib
 
 load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI()
 
 #create/load a Chroma client
 chroma_client = chromadb.PersistentClient(path = "vectordb")
